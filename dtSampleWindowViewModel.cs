a3df883dab71c8f4b7fc5a98ad5307934f1d409a
Mark Thorley <markt@DESKTOP-5PMK1JF>
2018-10-18 18:05:21 +0100

    testcase

--- dtSampleWindowViewModel.cs
+++ dtSampleWindowViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Dynamo.Core;
 using Dynamo.Extensions;
+using Dynamo.Graph.Nodes;
+using Dynamo.Graph.Workspaces;
+using Dynamo.Wpf.Extensions;
 
 namespace dtSampleExtension
 {
@@ -27,6 +30,62 @@ namespace dtSampleExtension
         {
             readyParams = p;
         }
+
+        #region Functions
+        public void FreezeSelected(dtSampleWindowViewModel se)
+        {
+            ReadyParams rp = se.ReadyParamType;
+            var vlp = rp as ViewLoadedParams;
+            var wm = rp.CurrentWorkspaceModel;
+
+            foreach (NodeModel node in wm.Nodes)
+            {
+                if (node.IsSelected)
+                {
+                    node.IsFrozen = true;
+                }
+            }
+        }
+
+        public void UnFreezeSelected(dtSampleWindowViewModel se)
+        {
+            ReadyParams rp = se.ReadyParamType;
+            var vlp = rp as ViewLoadedParams;
+            var wm = rp.CurrentWorkspaceModel;
+
+            foreach (NodeModel node in wm.Nodes)
+            {
+                if (node.IsSelected)
+                {
+                    node.IsFrozen = false;
+                }
+            }
+        }
+
+        public void FreezeAll(dtSampleWindowViewModel se)
+        {
+            ReadyParams rp = se.ReadyParamType;
+            var vlp = rp as ViewLoadedParams;
+            var wm = rp.CurrentWorkspaceModel;
+
+            foreach (NodeModel node in wm.Nodes)
+            {
+                node.IsFrozen = true;
+            }
+        }
+
+        public void UnFreezeAll(dtSampleWindowViewModel se)
+        {
+            ReadyParams rp = se.ReadyParamType;
+            var vlp = rp as ViewLoadedParams;
+            var wm = rp.CurrentWorkspaceModel;
+
+            foreach (NodeModel node in wm.Nodes)
+            {
+                node.IsFrozen = false;
+            }
+        }
+        #endregion
 
         public void Dispose()
         {
